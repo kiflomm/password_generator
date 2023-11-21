d025105e8527a56c0150c7836047116b0907b148
Kiflom Berihu Abay <144546891+kiflomm@users.noreply.github.com>
2023-11-21 10:05:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let display = document.getElementById('display');
+let copy = document.getElementById('copy');
+let lengthChoose = document.getElementsByName('length')[0];
+let uppercaseCheck = document.getElementsByName('uppercase')[0];
+let lowercaseCheck = document.getElementsByName('lowercase')[0];
+let numbersCheck = document.getElementsByName('numbers')[0];
+let symbolsCheck = document.getElementsByName('symbols')[0];
+let generate = document.getElementById("generate");
+
+let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+let numbers = '1234567890';
+let symbols = "!@#$%^&*()_+=";
+
+let password ='';
+function generater(){
+    password =''; 
+    for(;password.length<lengthChoose.value;){
+        if(uppercaseCheck.checked){
+            let index = Math.floor((Math.random())*26);
+            let divisible = Math.floor((Math.random()*100))%2;
+            password = password.concat((divisible?uppercase[index]:''));
+            if(password.length == lengthChoose.value) break;
+        }
+        if(lowercaseCheck.checked){
+            let index = Math.floor((Math.random())*26);
+            let divisible = Math.floor((Math.random()*100))%2;
+            password = password.concat((divisible?lowercase[index]:''));
+            if(password.length == lengthChoose.value) break;
+        }
+        if(numbersCheck.checked){
+            let index = Math.floor((Math.random())*10);
+            let divisible = Math.floor((Math.random()*100))%2;
+            password = password.concat((divisible?numbers[index]:''));
+            if(password.length == lengthChoose.value) break;
+        }
+        if(symbolsCheck.checked){
+            let index = Math.floor((Math.random())*13);
+            let divisible = Math.floor((Math.random()*100))%2;
+            password = password.concat((divisible?symbols[index]:''));
+            if(password.length == lengthChoose.value) break;
+        }
+    }
+    display.innerHTML = password;
+}
